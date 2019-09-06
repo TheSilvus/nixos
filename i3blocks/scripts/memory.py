277ae791c5cefdfcee50c062e5c1849243fd3902
TheSilvus <renkenor@gmail.com>
2019-09-06 20:15:24 +0200

    Fixed memory script

--- i3blocks/scripts/memory.py
+++ i3blocks/scripts/memory.py
@@ -8,24 +8,28 @@ def err():
     print("ERR")
     sys.exit(33)
 
-mem_total = subprocess.check_output(["sh", "-c", "cat /proc/meminfo | grep 'MemTotal'"]).decode("utf-8")
-mem_free = subprocess.check_output(["sh", "-c", "cat /proc/meminfo | grep 'MemFree'"]).decode("utf-8")
+free_mem = subprocess.check_output(["sh", "-c", "free | grep Mem"]).decode("utf-8")
+free_swap = subprocess.check_output(["sh", "-c", "free | grep Swap"]).decode("utf-8")
+
+def extract_numbers(s):
+    return [int(match) for match in re.findall("\\d+", s)]
 
 
-def extract_number(s):
-    match = re.search("\\d+", s)
-    if match:
-        return int(match.group(0))
-    else:
-        err()
+free_mem_numbers = extract_numbers(free_mem)
+free_swap_numbers = extract_numbers(free_swap)
 
-# Original data is in kB here, I think this means they are working with the 
-# metric system here
-mem_total = extract_number(mem_total) / 1000000
-mem_free = extract_number(mem_free) / 1000000
-mem_used = mem_total - mem_free
+KIBI_TO_GIBI = 1024 * 1024
 
-print(f"MEM {mem_used:.1f}GB/{mem_total:.1f}GB")
+mem_total = free_mem_numbers[0] / KIBI_TO_GIBI
+mem_available = free_mem_numbers[-1] / KIBI_TO_GIBI
+swap_total = free_swap_numbers[0] / KIBI_TO_GIBI
+swap_available = free_swap_numbers[-1] / KIBI_TO_GIBI
+
+mem_used = mem_total - mem_available
+swap_used = swap_total - swap_available
+
+
+print(f"MEM {mem_used:.1f}/{mem_total:.1f}GB SWA {swap_used:.1f}/{swap_total:.1f}GB")
 if mem_used > 0.9 * mem_total:
     sys.exit(33)
 
